4527f2c5f3e592ead6a52203c02795cb8fee4a48
popov-p <pashafizmat1@gmail.com>
2024-09-13 15:58:30 +0300

    transfer to another distro

--- src/main.c
+++ src/main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <antlr3.h>
+#include "TLexer.h"
+#include "TParser.h"
 
 int main(int argc, char** argv) {
-  printf("HELLO WORLD \n");
-  return 0;
+  if (argc != 2) {
+      fprintf(stderr, "Usage: %s <input file>\n", argv[0]);
+      return 1;
+    }
+
+  FILE *file = fopen(argv[1], "r");
+  if (!file) {
+      perror("fopen");
+      return 1;
+    }
+  //TLexer* lexer = TLexerNew(file);
 }
