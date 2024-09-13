6c85bc70306f40b466d52c3442834a1783c00d00
pavel <pavel@debian-spo.myguest.virtualbox.org>
2024-09-13 19:02:12 +0300

    proper compilation flags

--- src/main.c
+++ src/main.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
-#include <antlr3.h>
 #include "TLexer.h"
 #include "TParser.h"
 
 int main(int argc, char** argv) {
-  if (argc != 2) {
-      fprintf(stderr, "Usage: %s <input file>\n", argv[0]);
-      return 1;
-    }
 
-  FILE *file = fopen(argv[1], "r");
-  if (!file) {
-      perror("fopen");
-      return 1;
-    }
-  //TLexer* lexer = TLexerNew(file);
+  printf("i`m sobralsya finally \n");
+
+  return 0;
 }
