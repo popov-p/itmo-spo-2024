5eae08c907a76e450e2e00ca1b19b1e0eb75a64f
pavel <pavel@debian-spo.myguest.virtualbox.org>
2024-10-19 22:43:23 +0300

    basis

--- Lab-2/src/CFGBuilder.c
+++ Lab-2/src/CFGBuilder.c
@@ -1,24 +1,16 @@
 #include "CFGBuilder.h"
 
-//void writeNode(pANTLR3_BASE_TREE tree, FILE *file, int* id) {
-//    fprintf(file, "    node%d [label=\"%s\"];\n", *id, (char *)tree->toString(tree)->chars); //NOLINT
-//}
-
-//void writeEdge(FILE *file, int* parentId, int* childId) {
-//    fprintf(file, "    node%d -> node%d;\n", *parentId, *childId); //NOLINT
-//}
-
-void writeBlock(FILE *file, int num) {
+void writeBlock(int num, FILE *file) {
     fprintf(file, "    node%d [label=\"%d\"];\n", num, num); //NOLINT
 }
 
-//void writeCFGEdges(FILE *file, CFG* cfg) {
-//    for (int i = 0; i < cfg->block_count; i++) {
-//        for(int j = 0; i < cfg->blocks[i]->successor_count; j++) {
-//            fprintf(file, "    node%d [label=\"%d\"];\n", cfg->blocks[i]->id, cfg->blocks[i]->successors[j]); //NOLINT
-//        }
-//    }
-//}
+void writeCFGEdges(CFG* cfg, FILE *file) {
+    for (int i = 0; i < cfg->block_count; i++) {
+        for(int j = 0; j < cfg->blocks[i]->successor_count; j++) {
+            fprintf(file, "    node%d -> node%d;\n", i, cfg->blocks[i]->successors[j]); //NOLINT
+        }
+    }
+}
 
 void cfg_walker(CFG* cfg, pANTLR3_BASE_TREE_ADAPTOR adaptor, pANTLR3_BASE_TREE tree, int* lastBlockIndex) {
     if (adaptor == NULL) return;
@@ -44,32 +36,8 @@ void cfg_walker(CFG* cfg, pANTLR3_BASE_TREE_ADAPTOR adaptor, pANTLR3_BASE_TREE t
         pANTLR3_BASE_TREE_ADAPTOR func_name_node = adaptor->getChild(adaptor, tree, 0);
         printf("found CALL: %s\n", func_name_node->getText(func_name_node, func_name_node)->chars);
     }
-
-//    int childCount = adaptor->getChildCount(adaptor, tree);
-//    for (int i = 0; i < childCount; i++) {
-//        cfg_walker(cfg, adaptor, adaptor->getChild(adaptor, tree, i));
-//    }
 }
 
-//void generate_cfg(pANTLR3_BASE_TREE node, FILE *file) {
-//    CFG* cfg = createCFG();
-//    fprintf(file, "digraph G {\n"); //NOLINT
-//    int* id = (int*)malloc(sizeof(int)); //todo: encapsulate id into a cfg child count
-//    *id = 0;
-//    int start_id = *id;
-//    fprintf(file, "    node%d [label=\"%s\"];\n", *id, "Начало"); //NOLINT
-//    //cfg_walker(cfg, node, file, id);
-//    int end_id = ++(*id);
-//    fprintf(file, "    node%d [label=\"%s\"];\n", (*id)++, "Конец"); //NOLINT
-//    writeEdge(file, &start_id, &end_id);
-//    fprintf(file, "}\n"); //NOLINT
-//}
-
-//CFG* cfg_from_antlr_tree(pParseResult parseResult) {
-//   CFG* cfg = createCFG();
-//   cfg_walker(cfg, tree, id);
-//   return cfg; //NOLINT
-//}
 
 CFG* generateCFG(pParseResult parseResult) {
     CFG* cfg = initEmptyCFG();
@@ -82,31 +50,18 @@ CFG* generateCFG(pParseResult parseResult) {
 }
 
 void outputCFG(CFG* cfg, FILE* file) {
-    //CFG* cfg = cfg_from_antlr_tree(tree);
     fprintf(file, "digraph G {\n"); //NOLINT
     fprintf(file, "    start [label=\"%s\"];\n", "Начало"); //NOLINT
     fprintf(file, "    end [label=\"%s\"];\n", "Конец");
 
 
     for (int i = 0; i < cfg->block_count; i++) {
-        writeBlock(file, i);
+        writeBlock(i, file);
     }
-    //writeCFGEdges(file, cfg);
-    fprintf(file, "    start -> node%d;\n", cfg->blocks[0]->id);
-    fprintf(file, "    node%d -> end;\n", cfg->blocks[cfg->block_count-1]->id);
+    writeCFGEdges(cfg, file);
+    fprintf(file, "    start -> node%d;\n", 0);
+    fprintf(file, "    node%d -> end;\n", cfg->block_count-1);
     fprintf(file, "}\n"); //NOLINT
 }
 
-//void generate_parse_tree(pANTLR3_BASE_TREE tree, FILE *file) {
-//    fprintf(file, "digraph AST {\n"); //NOLINT
-//    fprintf(file, "    node [shape=box];\n"); //NOLINT
-
-//    int* id = (int*)malloc(sizeof(int));
-//    *id = 0;
-//    treeToDotNodes(tree, file, id);
-//    *id = 0;
-//    treeToDotEdges(tree, file, id);
-//    free(id);
-//    fprintf(file, "}\n"); //NOLINT
-//}
 
